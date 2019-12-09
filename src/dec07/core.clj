09106d7b413033275d311f5088ad2dbfcedf7785
Joel Dunham <jrwdunham@gmail.com>
2019-12-09 12:17:17 -0800

    Refactor of Day 7

--- src/dec07/core.clj
+++ src/dec07/core.clj
@@ -62,72 +62,63 @@
         99 (first inputs)))))
 
 (defn run-halting
-  "TODO: rename!"
+  "Run `program` at position `cur-pos` with `inputs` (vec[int]). Returns a 2-vec
+  where the first element is an output and the second is either `nil` (the
+  termination case) or a continuation function that takes a new input and
+  continues running the program from the position where it had previously
+  halted."
   ([program inputs] (run-halting program inputs 0))
   ([program inputs cur-pos]
-
-   #_(println
-    (format
-     (str "Running program\n%s\n"
-          "at position\n%s\n%s\n"
-          "with inputs\n%s\n") program cur-pos (nth program cur-pos) inputs))
-
    (let [[op p1 _ p3 v1 v2] (get-moded-vals program cur-pos)
          op-v1-v2->p3
-         (fn [op]
-           #_(println "Setting value at position %s to %s" p3 (op v1 v2))
-           (run-halting (assoc program p3 (op v1 v2)) inputs (+ cur-pos 4)))
-         op-v1->v2 (fn [op] (if (op 0 v1) (run-halting program inputs (+ cur-pos 3))
-                                (run-halting program inputs v2)))
+         (fn [op] (run-halting (assoc program p3 (op v1 v2)) inputs
+                               (+ cur-pos 4)))
+         op-v1->v2
+         (fn [op] (if (op 0 v1)
+                    (run-halting program inputs (+ cur-pos 3))
+                    (run-halting program inputs v2)))
          op-v1-v2->characteristic->p3
          (fn [op] (run-halting (assoc program p3 (if (op v1 v2) 1 0))
-                    inputs (+ cur-pos 4)))]
+                               inputs (+ cur-pos 4)))]
       (case op
         1 (op-v1-v2->p3 +)
         2 (op-v1-v2->p3 *)
-
-        3 (let [[first-input & rest-inputs] inputs]
-            #_(when-not rest-inputs (println "WARNING: HAVE NO MORE INPUTS\n"))
-            (run-halting (assoc program p1 first-input) rest-inputs (+ cur-pos 2)))
-
-        ;; 4 (run-halting program [v1] (+ cur-pos 2))
-        ;; 4 [v1 (fn [inputs] (run-halting program inputs (+ cur-pos 2)))]
-        ;; return an output continuation
-        4 (do
-            #_(println
-             (format (str "program\n%s\nis returning intermediate output\n%s\nand"
-                          " will restart at position\n%s\n%s\n"
-                          "with new inputs.\n\n") program v1 (+ cur-pos 2)
-                          (nth program (+ cur-pos 2))))
-            [:continuation v1 (fn [inputs] (run-halting program [inputs]
-                                                        (+ cur-pos 2)))])
-
+        3 (run-halting (assoc program p1 (first inputs)) (rest inputs)
+                       (+ cur-pos 2))
+        4 [v1 (fn [input] (run-halting program [input] (+ cur-pos 2)))]
         5 (op-v1->v2 =)
         6 (op-v1->v2 not=)
         7 (op-v1-v2->characteristic->p3 <)
         8 (op-v1-v2->characteristic->p3 =)
-        99 [:termination (first inputs) nil]))))
+        99 [(first inputs) nil]))))
 
-(defn x
+(defn run-program-in-feedback-loop
+  "Run `(count phase-settings)` instances of `program` in series, feeding phase
+  setting i to program i as first input. Input `input` will be provided as the
+  second argument to the first program and its output will be provided as the
+  second argument to the second program, etc. When a program returns an output,
+  it may optionally also return a continuation, a function that takes an input
+  and continues execution of the program from the last state. If the series
+  completes and continuations are available, the series, now represented in the
+  recursive call as a vector of continuation functions in `functions`, is run
+  again with the final output of the previous series' output being provided as
+  the intput to the new series. If no continuations are available, the final
+  program's output is returned."
   [program phase-settings functions input]
   (let [fns (or functions
                 (map (fn [ps] (fn [in] (run-halting program [ps in])))
                      phase-settings))
         {new-series :amplifiers output :in}
-        (reduce (fn [{:keys [in amplifiers] :as acc} f]
-                  #_(println "calling function" f)
-                  #_(println "on vector of in [in]" [in])
-                  (let [[state output continuation] (f in)]
-                    (cond
-                      (= state :termination) (assoc acc :in output)
-                      output (-> acc
-                                 (assoc :in output)
-                                 (update :amplifiers conj continuation))
-                      :else (update acc :amplifiers conj continuation))))
+        (reduce (fn [{:keys [in] :as acc} f]
+                  (let [[output continuation] (f in)
+                        new-acc (assoc acc :in output)]
+                    (if continuation
+                      (update new-acc :amplifiers conj continuation)
+                      new-acc)))
                 {:in input :amplifiers []}
                 fns)]
     (if (seq new-series)
-      (x nil nil new-series output)
+      (run-program-in-feedback-loop nil nil new-series output)
       output)))
 
 (defn run-amplifiers
@@ -146,8 +137,12 @@
 
 (defn find-highest-feedback-loop-signal
   [program]
-  (-> (for [a (range 5 10) b (range 5 10) c (range 5 10) d (range 5 10) e (range 5 10)
+  (-> (for [a (range 5 10)
+            b (range 5 10)
+            c (range 5 10)
+            d (range 5 10)
+            e (range 5 10)
             :when (= (sort [a b c d e]) (sort (vec (set [a b c d e]))))]
-        (x program [a b c d e] nil 0))
+        (run-program-in-feedback-loop program [a b c d e] nil 0))
       sort
       last))
